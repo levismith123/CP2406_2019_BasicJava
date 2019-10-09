386561c99816342593171794d978cf30e7119983
levismith123 <37235968+levismith123@users.noreply.github.com>
2019-10-09 13:14:26 +1000

    Successfully made button disable when clicked

--- src/week9/JFrameDisableButton.java
+++ src/week9/JFrameDisableButton.java
@@ -3,12 +3,28 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class JFrameDisableButton {
+public class JFrameDisableButton extends JFrame implements ActionListener {
 
-    JFrame frame = new JFrame("Frame");
+
     JButton button = new JButton("Disable");
+    final int HEIGHT = 100;
+    final int WIDTH = 250;
+
+    JFrameDisableButton(){
+        super("Wassup");
+        setSize(WIDTH, HEIGHT);
+        setLayout(new FlowLayout());
+        add(button);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        button.addActionListener(this);
+    }
 
     public static void main(String[] args){
+        new JFrameDisableButton().show();
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        button.setEnabled(false);
     }
 }
